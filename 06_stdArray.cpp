30ce9219ef7e1cc4164b09ad3ec1369bc84a23f2
taemin-steve <okchrisok@naver.com>
2024-09-22 23:51:57 +0900

    feat: Add == operation

--- 06_stdArray.cpp
+++ 06_stdArray.cpp
@@ -5,8 +5,13 @@ using namespace std;
 
 int main(){
     array<int, 5> arr{1,2,3};
+    array<int, 5> arr2{1,2,3};
 
     cout << arr.size() << endl;
     cout << arr.at(2) << endl;
+
+    if(arr == arr2){
+        cout << "모두 같음" << endl;
+    }
     
 }
